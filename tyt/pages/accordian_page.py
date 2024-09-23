0018942f7d51ee62741f2a827e8c27ec61a5c79c
Andrew <varlamov30.1987@gmail.com>
2024-09-23 11:47:28 +0400

    editing

--- tyt/pages/accordian_page.py
+++ tyt/pages/accordian_page.py
@@ -18,7 +18,7 @@ class AccordianPage(BasePage):
     def click_what_card(self):
         self.wait.until(EC.element_to_be_clickable(self.WHAT_CARD))
         value_title = self.driver.find_element("xpath", '//*[text()="What is Lorem Ipsum?"]').text
-        assert value_title == "What is Lorem Ipsum?", "Заголовок не совпадает"
+        assert "What is Lorem Ipsum?" in value_title, "Заголовок не совпадает"
         value_body_title = self.driver.find_element("xpath", '//*[@id="section1Content"]/p').text
         print(value_body_title)
         assert "Lorem Ipsum is simply dummy text of the printing and typ" in value_body_title, "Содержимое не совпадает"
@@ -26,7 +26,7 @@ class AccordianPage(BasePage):
     def click_where_card(self):
         self.wait.until(EC.element_to_be_clickable(self.WHERE_CARD)).click()
         value_title = self.driver.find_element("xpath", '//*[text()="Where does it come from?"]').text
-        assert value_title == "Where does it come from?", "Заголовок не совпадает"
+        assert "Where does it come from?" in value_title, "Заголовок не совпадает"
         value_body_title = self.driver.find_element("xpath", '//*[@id="section2Content"]/p').text
         print(value_body_title)
         assert "Contrary to popular belief, Lorem Ipsum is not simply ra" in value_body_title, "Содержимое не совпадает"
@@ -34,7 +34,7 @@ class AccordianPage(BasePage):
     def click_why_card(self):
         self.wait.until(EC.element_to_be_clickable(self.WHY_CARD)).click()
         value_title = self.driver.find_element("xpath", '//*[text()="Why do we use it?"]').text
-        assert value_title == "Why do we use it?", "Заголовок не совпадает"
+        assert "Why do we use it?" in value_title, "Заголовок не совпадает"
         value_body_title = self.driver.find_element("xpath", '//*[@id="section3Content"]/p').text
         print(value_body_title)
         assert "It is a long established fact that a reader will be dist" in value_body_title, "Содержимое не совпадает"
--- tyt/pages/droppable_page.py
+++ tyt/pages/droppable_page.py
@@ -34,121 +34,124 @@ class DroppablePage(BasePage):
         self.wait.until(EC.text_to_be_present_in_element(self.MAIN_WORD, 'Droppable')), "Текст не совпадает"
 
     def drag_me_in_drop_here(self):
+        # Проверяем текст элемента
         drag_me = self.wait.until(EC.element_to_be_clickable(self.DRAG_ME)).text
+        assert "Drag me" in drag_me, "название не совпадает"
+        # Проверяем текст элемента
         drop_here = self.wait.until(EC.element_to_be_clickable(self.DROP_HERE)).text
-        assert "Drag me" in drag_me
-        assert "Drop here" in drop_here
+        assert "Drop here" in drop_here, "название не совпадает"
         drag_me = self.wait.until(EC.element_to_be_clickable(self.DRAG_ME))
         drop_here = self.wait.until(EC.element_to_be_clickable(self.DROP_HERE))
+        # Передвигаем элемент drag me в область элемента drop here
         self.drag_and_drop(drag_me, drop_here)
         drop_here = self.wait.until(EC.element_to_be_clickable(self.DROP_HERE)).text
-        assert "Dropped!" in drop_here
+        assert "Dropped!" in drop_here, "название не совпадает"
 
     def drag_not_acceptable_in_drop_here(self):
         # Переключаемся на вкладку Accept
         self.wait.until(EC.element_to_be_clickable(self.ACCEPT_TAB)).click()
         # Проверяем текст элемента
         not_acceptable = self.wait.until(EC.element_to_be_clickable(self.NOT_ACCEPTABLE)).text
-        assert "Not Acceptable" in not_acceptable
+        assert "Not Acceptable" in not_acceptable, "название не совпадает"
         not_acceptable = self.wait.until(EC.element_to_be_clickable(self.NOT_ACCEPTABLE))
         drop_here = self.wait.until(EC.element_to_be_clickable(self.ACCEPT_DROP_HERE))
         # Передвигаем элемент Not acceptable в Drop here и проверяем, что текстовое значение Drop here не изменилось
         self.drag_and_drop(not_acceptable, drop_here)
         drop_here = self.wait.until(EC.element_to_be_clickable(self.ACCEPT_DROP_HERE)).text
-        assert "Drop here" in drop_here
+        assert "Drop here" in drop_here, "название не совпадает"
 
     def drag_acceptable_in_drop_here(self):
         # Проверяем текст элемента
         acceptable = self.wait.until(EC.element_to_be_clickable(self.ACCEPTABLE)).text
-        assert "Acceptable" in acceptable
+        assert "Acceptable" in acceptable, "название не совпадает"
         acceptable = self.wait.until(EC.element_to_be_clickable(self.ACCEPTABLE))
         drop_here = self.wait.until(EC.element_to_be_clickable(self.ACCEPT_DROP_HERE))
         # Передвигаем элемент Acceptable в Drop here и проверяем, что текстовое значение Drop here изменилось "Dropped!"
         self.drag_and_drop(acceptable, drop_here)
         drop_here = self.wait.until(EC.element_to_be_clickable(self.ACCEPT_DROP_HERE)).text
-        assert "Dropped!" in drop_here
+        assert "Dropped!" in drop_here, "название не совпадает"
 
     def drag_me_in_outer_droppable_ng(self):
         # Переключаемся на вкладку Prevent Propogation
         self.wait.until(EC.element_to_be_clickable(self.PREVENT_PROPOGATION_TAB)).click()
         drag_me = self.wait.until(EC.element_to_be_clickable(self.PREVENT_DRAG_ME)).text
-        assert "Drag Me" in drag_me
+        assert "Drag Me" in drag_me, "название не совпадает"
         outer_droppable = self.wait.until(EC.element_to_be_clickable(self.OUTER_DROPPABLE_NG)).text
-        assert "Outer droppable" in outer_droppable
+        assert "Outer droppable" in outer_droppable, "название не совпадает"
         # Передвигаем элемент Drag Me в Outer droppable и проверяем,
                                                           # что текстовое значение Outer droppable изменилось "Dropped!"
         self.action_drag_and_drop_by_offset(self.wait.until
                                             (EC.element_to_be_clickable(self.PREVENT_DRAG_ME)), 320, 0)
         outer_droppable = self.wait.until(EC.element_to_be_clickable(self.OUTER_DROPPABLE_NG)).text
-        assert "Dropped!" in outer_droppable
+        assert "Dropped!" in outer_droppable, "название не совпадает"
 
     def drag_me_in_inner_droppable_not_greedy(self):
         # Переключаемся на вкладку Prevent Propogation
         self.wait.until(EC.element_to_be_clickable(self.PREVENT_PROPOGATION_TAB)).click()
         drag_me = self.wait.until(EC.element_to_be_clickable(self.PREVENT_DRAG_ME)).text
-        assert "Drag Me" in drag_me
+        assert "Drag Me" in drag_me, "название не совпадает"
         inner_droppable = self.wait.until(EC.element_to_be_clickable(self.INNER_DROPPABLE_NG)).text
-        assert "Inner droppable (not greedy)" in inner_droppable
+        assert "Inner droppable (not greedy)" in inner_droppable, "название не совпадает"
         # Передвигаем элемент Drag Me в Inner droppable (not reedy) и проверяем,
                                                           # что текстовое значение Inner droppable изменилось "Dropped!"
         self.action_drag_and_drop_by_offset(self.wait.until
                                             (EC.element_to_be_clickable(self.PREVENT_DRAG_ME)), 0, 120)
         inner_droppable = self.wait.until(EC.element_to_be_clickable(self.INNER_DROPPABLE_NG)).text
-        assert "Dropped!" in inner_droppable
+        assert "Dropped!" in inner_droppable, "название не совпадает"
 
     def drag_me_in_outer_droppable_g(self):
         # Переключаемся на вкладку Prevent Propogation
         self.wait.until(EC.element_to_be_clickable(self.PREVENT_PROPOGATION_TAB)).click()
         drag_me = self.wait.until(EC.element_to_be_clickable(self.PREVENT_DRAG_ME)).text
-        assert "Drag Me" in drag_me
+        assert "Drag Me" in drag_me, "название не совпадает"
         outer_droppable = self.wait.until(EC.element_to_be_clickable(self.OUTER_DROPPABLE_G)).text
-        assert "Outer droppable" in outer_droppable
+        assert "Outer droppable" in outer_droppable, "название не совпадает"
         # Передвигаем элемент Drag Me в Outer droppable и проверяем,
                                                           # что текстовое значение Outer droppable изменилось "Dropped!"
         self.action_drag_and_drop_by_offset(self.wait.until
                                             (EC.element_to_be_clickable(self.PREVENT_DRAG_ME)), 0, 170)
         outer_droppable = self.wait.until(EC.element_to_be_clickable(self.OUTER_DROPPABLE_G)).text
-        assert "Dropped!" in outer_droppable
+        assert "Dropped!" in outer_droppable, "название не совпадает"
 
     def drag_me_in_inner_droppable_g(self):
         # Переключаемся на вкладку Prevent Propogation
         self.wait.until(EC.element_to_be_clickable(self.PREVENT_PROPOGATION_TAB)).click()
         drag_me = self.wait.until(EC.element_to_be_clickable(self.PREVENT_DRAG_ME)).text
-        assert drag_me == "Drag Me"
+        assert drag_me == "Drag Me", "название не совпадает"
         inner_droppable = self.wait.until(EC.element_to_be_clickable(self.INNER_DROPPABLE_G)).text
-        assert "Inner droppable (greedy)" in inner_droppable
+        assert "Inner droppable (greedy)" in inner_droppable, "название не совпадает"
         # Передвигаем элемент Drag Me в Inner droppable и проверяем,
                                                           # что текстовое значение Inner droppable изменилось "Dropped!"
         self.action_drag_and_drop_by_offset(self.wait.until
                                             (EC.element_to_be_clickable(self.PREVENT_DRAG_ME)), 0, 65)
         inner_droppable = self.wait.until(EC.element_to_be_clickable(self.INNER_DROPPABLE_G)).text
-        assert "Dropped!" in inner_droppable
+        assert "Dropped!" in inner_droppable, "название не совпадает"
 
     def not_revert_in_drop_here(self):
         # Переключаемся на вкладку Prevent Draggable
         self.wait.until(EC.element_to_be_clickable(self.REVERT_DRAGGABLE_TAB)).click()
         not_revert = self.wait.until(EC.element_to_be_clickable(self.NOT_REVERT)).text
         drop_here = self.wait.until(EC.element_to_be_clickable(self.DROP_HERE_REVERT_DRAGGABLE)).text
-        assert "Not Revert" in not_revert
-        assert "Drop here" in drop_here
+        assert "Not Revert" in not_revert, "название не совпадает"
+        assert "Drop here" in drop_here, "название не совпадает"
         not_revert = self.wait.until(EC.element_to_be_clickable(self.NOT_REVERT))
         drop_here = self.wait.until(EC.element_to_be_clickable(self.DROP_HERE_REVERT_DRAGGABLE))
         self.drag_and_drop(not_revert, drop_here)
         drop_here = self.wait.until(EC.element_to_be_clickable(self.DROP_HERE_REVERT_DRAGGABLE)).text
-        assert "Dropped!" in drop_here
+        assert "Dropped!" in drop_here, "название не совпадает"
 
     def will_revert_in_drop_here(self):
         # Переключаемся на вкладку Prevent Draggable
         self.wait.until(EC.element_to_be_clickable(self.REVERT_DRAGGABLE_TAB)).click()
         will_revert = self.wait.until(EC.element_to_be_clickable(self.WILL_REVERT)).text
         drop_here = self.wait.until(EC.element_to_be_clickable(self.DROP_HERE_REVERT_DRAGGABLE)).text
-        assert "Will Revert" in will_revert
-        assert "Dropped!" in drop_here
+        assert "Will Revert" in will_revert, "название не совпадает"
+        assert "Dropped!" in drop_here, "название не совпадает"
         will_revert = self.wait.until(EC.element_to_be_clickable(self.WILL_REVERT))
         drop_here = self.wait.until(EC.element_to_be_clickable(self.DROP_HERE_REVERT_DRAGGABLE))
         self.drag_and_drop(will_revert, drop_here)
         drop_here = self.wait.until(EC.element_to_be_clickable(self.DROP_HERE_REVERT_DRAGGABLE)).text
-        assert "Dropped!" in drop_here
+        assert "Dropped!" in drop_here, "название не совпадает"
 
 
 
